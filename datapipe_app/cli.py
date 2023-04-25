c44639ba98952d8f6dcd99c292c0d1252571c86e
Shakirov Renat <renatshk@gmail.com>
2023-04-25 19:37:09 +0400

    added ways to set up host and port for uvicorn app

--- datapipe_app/cli.py
+++ datapipe_app/cli.py
@@ -344,13 +344,15 @@ def run(ctx: click.Context, loop: bool, loop_delay: int) -> None:
 
 
 @cli.command()
+@click.option("--host", type=click.STRING, default="0.0.0.0")
+@click.option("--port", type=click.INT, default=8000)
 @click.pass_context
-def api(ctx: click.Context) -> None:
+def api(ctx: click.Context, host: str, port: str) -> None:
     app: DatapipeApp = ctx.obj["pipeline"]
 
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0")
+    uvicorn.run(app, host=host, port=port)
 
 
 def main():
